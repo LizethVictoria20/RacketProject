764de8bc811aa866417788b96a41106978b57289
LizethVictoria20 <lizvictoria754@gmail.com>
2024-03-01 11:55:58 -0500

    basic concepts

--- extract.rkt
+++ extract.rkt
@@ -2,7 +2,18 @@
  
 ;(substring "the boy out of the country" 4 7)
 
-"Lizeth"
+; It's true or false
+#t
+#f
+
+;Simple text wir duoble quote
+"Shhhhh"
+
+;Define a variable with a number value
+(define age 93)
+
+;Define square 
+(sqrt 4)
 
 
 ; command /Applications/Racket\ v8.11.1/bin/racket extract.rkt
