9f9c86df3902f380947792e244ebde97309a1af8
LizethVictoria20 <lizvictoria754@gmail.com>
2024-03-08 10:18:23 -0500

    functions with parameters/no parameters

---
+++ functions.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+;Escribir una función que imprima un mensaje de bienvenida por pantalla.
+
+(define (welcomeMessage)
+    (displayln "Holis")
+)
+
+(welcomeMessage)
+
+(displayln "holis")
+
+;Escribir una función que imprima tu nombre completo con dos var de función
+
+(define (myName)
+    (define firstName "Liz")
+    (define lastName " Victoria")
+    (display firstName)
+    (displayln lastName)
+)
+(myName)
+
+
+;Escribir una función que calcule el área de un triángulo dados su base y altura.
+(define (triangleArea base height)
+    (define operation (/( * base height) 2 )
+    )
+    operation
+)
+
+(triangleArea 4 6)
+
+
+;Escribir una función que calcule el perímetro de un cuadrado
+
+(define (squarePerimeter perimeter)
+    (define operation2 ( * perimeter 4)
+    ) 
+    operation2
+)
+
+(squarePerimeter 2)
+
+;Escribir una función que convierta una temperatura en grados Fahrenheit a grados Celsius.
+
+(define (temperature fahrenheit )
+    (define convertion (/(- fahrenheit 32)5/9))convertion)
+(temperature 32)
