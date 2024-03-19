550ba287ebd9496fb4b2604d78c87a534e6a4b13
LizethVictoria20 <lizvictoria754@gmail.com>
2024-03-18 21:40:12 -0500

    Validando si que tipo de dato es

--- conditionals.rkt
+++ conditionals.rkt
@@ -4,114 +4,131 @@
 Disyunción al menos una es verdadera para que sea igual a verdad
 Conjunción si o si ambas deben de ser verdaderas
 |#
-;  (and (number? 3)(not (number? "3"))) #t
-;  (and (= 3 8)(not (number? "3"))) #f
-;  (or (= 3 8)(not (number? "3"))) #t
-;  (or (= 3 8)(string? '3)) #f
+ (and (number? 3)(not (number? "3"))) #t
+ (and (= 3 8)(not (number? "3"))) #f
+ (or (= 3 8)(not (number? "3"))) #t
+ (or (= 3 8)(string? '3)) #f
 
 
-; ;condicionales
-;  (define salario 1000)
-;  (and ( < salario 2000) (> salario 1500 ))
+;condicionales
+ (define salario 1000)
+ (and ( < salario 2000) (> salario 1500 ))
 
 
-;  #|
-;  Definir funcion que reciba uan parametro n
-;  Utilizar los ejemplos de condiciones que estan en la diapositiva
-;  [(< n 10) 5.0]
-;  |#
+ #|
+ Definir funcion que reciba uan parametro n
+ Utilizar los ejemplos de condiciones que estan en la diapositiva
+ [(< n 10) 5.0]
+ |#
 
-;  (define (greaterThan n)
-;      (cond
-;          [(< n 10) 5.0]
-;          [(< n 20) 5]
-;          [(< n 30) true]
-;          [else (display "n es menor")]
-;      )
-;  )
-;  (greaterThan (read))
-
-
-; #|
-; Definir funcion que tome 4 parametros para sacar el promedio porcentual de una materia 
-; |#
+ (define (greaterThan n)
+     (cond
+         [(< n 10) 5.0]
+         [(< n 20) 5]
+         [(< n 30) true]
+         [else (display "n es menor")]
+     )
+ )
+ (greaterThan (read))
 
 
-; (define (notaFinal materia primerParcial segundoParcial quiz)
-;     (cond
-;         [(symbol=? materia 'Calculo) 
-;             (define promedio (+ (* primerParcial 0.3) (* segundoParcial 0.3) (* quiz 0.4)))
-;             (display promedio)
-;         ]
-;         [(symbol=? materia 'Ingles) 
-;             (define promedio (+ (* primerParcial 0.5) (* segundoParcial 0.2) (* quiz 0.3)))
-;             (display promedio)
-;         ]
-;         [(symbol=? materia 'Deportes) 
-;             (define promedio (+ (* primerParcial 0.7) (* segundoParcial 0.2) (* quiz 0.1)))
-;             (display promedio)
-;         ]
-
-;         [else (display "Ingrese una asignatura correcta")]
-;     )
-; )
+#|
+Definir funcion que tome 4 parametros para sacar el promedio porcentual de una materia 
+|#
 
 
-; (display "Ingrese la materia que quiere promediar: ")
-; (define materia (read))
-; (display "Ingrese la primera nota del primer parcial: ")
-; (define primerParcial (read))
-; (display "Ingrese la segunda nota del segundo parcial: ")
-; (define segundoParcial (read))
-; (display "Ingrese la nota del parcial final : ")
-; (define parcialFinal (read))
+(define (notaFinal materia primerParcial segundoParcial quiz)
+    (cond
+        [(symbol=? materia 'Calculo) 
+            (define promedio (+ (* primerParcial 0.3) (* segundoParcial 0.3) (* quiz 0.4)))
+            (display promedio)
+        ]
+        [(symbol=? materia 'Ingles) 
+            (define promedio (+ (* primerParcial 0.5) (* segundoParcial 0.2) (* quiz 0.3)))
+            (display promedio)
+        ]
+        [(symbol=? materia 'Deportes) 
+            (define promedio (+ (* primerParcial 0.7) (* segundoParcial 0.2) (* quiz 0.1)))
+            (display promedio)
+        ]
+
+        [else (display "Ingrese una asignatura correcta")]
+    )
+)
 
 
-; (notaFinal materia primerParcial segundoParcial parcialFinal)
+(display "Ingrese la materia que quiere promediar: ")
+(define materia (read))
+(display "Ingrese la primera nota del primer parcial: ")
+(define primerParcial (read))
+(display "Ingrese la segunda nota del segundo parcial: ")
+(define segundoParcial (read))
+(display "Ingrese la nota del parcial final : ")
+(define parcialFinal (read))
+
+
+(notaFinal materia primerParcial segundoParcial parcialFinal)
 
 
 
-; #|
-; Misma funcion de arriba pero definida de una manera menos eficiente
-; |#
+#|
+Misma funcion de arriba pero definida de una manera menos eficiente
+|#
 
-; (define (notaFinal)
-;     (display "Ingresa el nombre de la materia: ")
-;     (define calculo 'M1)
-;     (define ingles 'M2)
-;     (define fundamentos 'M3)
-;     (define deportes 'M4)
-;     (define codigoMateria (read))(newline)
-;     (display "Ingresar nota parcial 1: ")
-;     (define notaP1 (read))(newline)
-;     (display "Ingresar nota parcial 2: ")
-;     (define notaP2 (read))(newline)
-;     (display "Ingresar nota parcial 3: ")
-;     (define notaP3 (read))(newline)
-;     (display "Ingresar nota parcial 4: ")
-;     (define notaP4 (read))(newline)
-;     (display "Ingresar nota quiz: ")
-;     (define quiz (read))(newline)
-;     (display "Esta es tu nota final: ")
+(define (notaFinal)
+    (display "Ingresa el nombre de la materia: ")
+    (define calculo 'M1)
+    (define ingles 'M2)
+    (define fundamentos 'M3)
+    (define deportes 'M4)
+    (define codigoMateria (read))(newline)
+    (display "Ingresar nota parcial 1: ")
+    (define notaP1 (read))(newline)
+    (display "Ingresar nota parcial 2: ")
+    (define notaP2 (read))(newline)
+    (display "Ingresar nota parcial 3: ")
+    (define notaP3 (read))(newline)
+    (display "Ingresar nota parcial 4: ")
+    (define notaP4 (read))(newline)
+    (display "Ingresar nota quiz: ")
+    (define quiz (read))(newline)
+    (display "Esta es tu nota final: ")
 
-;     (cond
-;         [(symbol=? codigoMateria 'M1)
-;             (+ (+ (* notaP1 0.3) (* notaP2 0.3) (* quiz 0.4)))
-;         ]
-;         [(symbol=? codigoMateria 'M2)
-;             (+ (+ (* notaP1 0.3) (* notaP2 0.3) (* quiz 0.4)))
-;         ]
-;         [(symbol=? codigoMateria 'M3)
-;             (+ (+ (* notaP1 0.3) (* notaP2 0.3) (* quiz 0.4)))
-;         ]
-;     )
-; )
-; (notaFinal)
+    (cond
+        [(symbol=? codigoMateria 'M1)
+            (+ (+ (* notaP1 0.3) (* notaP2 0.3) (* quiz 0.4)))
+        ]
+        [(symbol=? codigoMateria 'M2)
+            (+ (+ (* notaP1 0.3) (* notaP2 0.3) (* quiz 0.4)))
+        ]
+        [(symbol=? codigoMateria 'M3)
+            (+ (+ (* notaP1 0.3) (* notaP2 0.3) (* quiz 0.4)))
+        ]
+    )
+)
+(notaFinal)
+
+
+(define (validarNumero)
+    (displayln "Ingrese tu edad: ")
+    (define numero (read))
+    (cond
+        [(number? numero)
+            (EsMayor numero)
+        ]
+        [else 
+            (displayln "No es un número, ingrese un valor correcto")
+            (validarNumero)
+        ]
+    )
+)
+
 
 #|
 Escribe un programa que verifique si una persona es mayor de edad. 
 Considera la mayoría de edad a partir de los 18 años.
 |#
+
 (define (EsMayor edad)
     (cond 
         [(>= edad 18)
@@ -120,9 +137,41 @@ Considera la mayoría de edad a partir de los 18 años.
         [else
             (display "No eres mayor de edad")
         ]
+        
     )
 )
-(display "Ingresa tu edad: ")
-(define ingresarEdad (read))
-(EsMayor ingresarEdad)
+; (define ingresarEdad (read))
+(validarNumero)
 
+
+
+#|
+Desarrolla una función que clasifique un número como ‘positivo’, ‘negativo’ o ‘cero’.
+|#
+(define (ClasificarNumero number)
+    (cond
+        [(> number 0)
+            (display "El numero ")
+            (display number)
+            (display " es positivo")
+        ]
+        [(< number 0)
+            (display "El numero ")
+            (display number)
+            (display " es negativo")
+        ]
+        [(= number 0)
+            (display "El numero ")
+            (display number)
+            (display " es cero")
+        ]
+        [else
+            (display "Ingresa un número correcto ")
+        ]
+
+    )
+
+)
+(display "Ingresa numero: ")
+(define numero (read))
+(ClasificarNumero numero)
